db63a64f1dbae3640b970c0001e9e455eefe84d9
owtysm <cuoreui@gmail.com>
2025-11-02 17:47:25 +0100

    Add project files.

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("nugetpackCuore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("nugetpackCuore")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("51680bea-8cda-4de2-9af4-1ca5c5c0d230")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace nugetpackCuore
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            using (StreamWriter outputFile = new StreamWriter("output.log"))
+            {
+                if (!File.Exists("CuoreUI.Winforms.nuspec") || !File.Exists("nuget.exe"))
+                {
+                    outputFile.WriteLine("\"CuoreUI.Winforms.nuspec\" or \"nuget.exe\" not found!");
+                    return;
+                }
+                outputFile.WriteLine("needed files are present\n");
+
+                Console.WriteLine("If this is a revision, provide the revision number: (default: 0)");
+                Console.Write(" > ");
+
+                int revision = 0;
+                int.TryParse(Console.ReadLine(), out revision);
+                outputFile.WriteLine($"revision number provided is {revision}");
+
+                outputFile.WriteLine("scanning the nuspec..");
+                string[] contents = File.ReadAllLines("CuoreUI.Winforms.nuspec");
+                int index = 0;
+                foreach (string str in contents)
+                {
+                    if (str.TrimStart().StartsWith("<version>"))
+                    {
+                        outputFile.WriteLine("overwriting version line");
+                        string revisionStr = revision == 0 ? string.Empty : $".{revision}";
+                        contents[index] = $"    <version>{DateTime.Now.ToString("yyyy.MM.dd")}{revisionStr}</version>";
+                        outputFile.WriteLine($"\nnew content:\n{contents[index]}\n");
+                    }
+                    ++index;
+                }
+
+                outputFile.WriteLine("saving the new .nuspec");
+                File.WriteAllLines("CuoreUI.Winforms.nuspec", contents);
+
+                Process process = new Process()
+                {
+                    StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = "nuget.exe",
+                        Arguments = "pack",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.OutputDataReceived += (DataReceivedEventHandler)((sender, args) =>
+                {
+                    if (args.Data == null)
+                        return;
+                    outputFile.WriteLine(args.Data);
+                });
+                process.ErrorDataReceived += (DataReceivedEventHandler)((sender, args) =>
+                {
+                    if (args.Data == null)
+                        return;
+                    outputFile.WriteLine(args.Data);
+                });
+                outputFile.WriteLine("packing into .nupkg..");
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+                outputFile.WriteLine("finished");
+            }
+        }
+    }
+}
